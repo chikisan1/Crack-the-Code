ed57eb9eb01f91c44baf2ee6c496e70be1e33f3d
chikisan1 <chikaomaeda1@gmail.com>
2017-06-15 15:07:53 -0400

    String Branch

--- RMatrix/RMatrix.java
+++ RMatrix/RMatrix.java
@@ -4,11 +4,29 @@ public class RMatrix{
 	write a method to rotate by 90 degrees. Can you this in place?*/
 
 	public static char[][] rmatrix(char [][] matrix){
+		int i = 0; //incremeant down
+		int j = 0; //incremeant right 
+		int k = matrix.length - 1; //decremeant up 
+		int l = matrix.length - 1; //decremeant left
+		
+
 		for(int i = 0; i < matrix.length; i++){
-			for(int j = 0; j < matrix.legnth; j++){
-				char hold = matrix[][j];
-				char replace = matrix[i][j];
-			}
+
+			char right = matrix[m][j];
+			char down = matrix[i][m];
+			char left = matrix[n][l];
+			char up = matrix[k][n];
+
+			matrix[m][j] = up;
+			matrix[i][m] = right;
+			matrix[n][l] = down;
+			matrix[k][n] = left;
+
+			i++;
+			j++;
+			k--;
+			l--;
+
 		}
 	}
 
